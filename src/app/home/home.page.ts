cb4ee968537fedb7c312ef153e8e41fc37bc8600
gilsonpolito <gilsonpolito@gmail.com>
2019-03-11 22:21:35 -0300

    Implementacao de loading e alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { Carro } from '../modelos/Carro';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +11,36 @@ import { Carro } from '../modelos/Carro';
 export class HomePage implements OnInit{
   public carros: Carro[];
 
-  constructor(public http:HttpClient){
-
+  constructor(public http:HttpClient,
+              private loadingCtrl:LoadingController,
+              private alertCtrl:AlertController){
   }
   
-  ngOnInit(): void {
+  async ngOnInit() {
+    const loading = await this.loadingCtrl.create({
+      message:'Aguarde enquanto os carros são carregados...'
+    });
+
+    await loading.present();
+
     this.http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
     .subscribe(
       (carros)=>{
         this.carros = carros;
+      },
+      async (err: HttpErrorResponse)=>{
+        console.log('Deu erro ' + err.status);
+        const al = await this.alertCtrl.create({
+          header:'Erro!',
+          message: 'Erro ao listar carros',
+          buttons: [{text: 'OK'}]
+        });
+
+        await al.present();
+      }
+    ).add(
+      ()=>{
+        loading.dismiss();
       }
     )
   }
